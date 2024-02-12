b2d47039a6b2a9445f04e33c5a05eb7d9e3072cf
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-12 16:18:04 +0200

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 import './assets/js/main'
 import { routes } from './routes/routes'
 import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Sprite } from './components/IconSprite/Sprite';
 
 export const App = () => {
   const [routesList] = useState(routes())
 
   return (
     <>
+      <Sprite/>
+    
+      <Header/>
+
       <Routes>
         {routesList.map(route => <Route key={route.path} element={route.element} path={route.path}/>)}
       </Routes>
-      <h1>Start Training Equipment</h1>
     </>
   );
 }
