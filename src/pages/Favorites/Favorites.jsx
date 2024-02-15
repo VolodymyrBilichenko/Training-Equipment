5414aba74b376acb268a30be7ee00f2179357098
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-15 22:06:11 +0200

    Update Favorites.jsx

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { BackGroundDecor } from '../../components/BackGroundDecor/BackGroundDecor'
+import { BreadCrumbs } from '../../components/BreadCrumbs/BreadCrumbs'
+import { SectionTitle } from '../../components/SectionTitle/SectionTitle'
+import { ProductsList } from '../../components/ProductsList/ProductsList'
+import { PaginationProducts } from '../../components/PaginationProducts/PaginationProducts'
 
 export const Favorites = () => {
     return (
-        <div>Favorites</div>
+        <>
+            <BackGroundDecor/>
+
+            <BreadCrumbs pages={[{page: 'Избранное'}]}/>
+
+            <div className='favorites container'>
+
+                <SectionTitle title={'Избранное'}/>
+
+                <a href="#favorites-popup" className="favorites__receive_email open-popup">
+                    Желаю получить Коммерческое предложение по Email для товаров в Избранное
+                </a>
+
+                <ProductsList ClassNameList={'favorites__list'}/>
+
+                <PaginationProducts ClassName={'favorites__pagination'}/>
+            </div>
+        </>
     )
 }
