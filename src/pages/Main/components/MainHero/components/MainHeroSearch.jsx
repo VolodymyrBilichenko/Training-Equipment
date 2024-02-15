b467d26565cc9a3040397bf93593aff132ebc99e
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-15 20:16:07 +0200

    Update MainHeroSearch.jsx

--- src/pages/Main/components/MainHero/components/MainHeroSearch.jsx
+++ src/pages/Main/components/MainHero/components/MainHeroSearch.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const MainHeroSearch = ({searchPlaceholder}) => {
-  return (
-    <search className="index-hero__search">
-        <form className="index-hero__search--form" method="post">
-            <label>
-                <input type="search" name="search" placeholder={searchPlaceholder}/>
-            </label>
-            <button type="submit" title="Пошук">
-                <svg width="26" height="26" viewBox="0 0 48 48">
-                    <use xlinkHref="#search"></use>
-                </svg>
-            </button>
-        </form>
-    </search>
-  )
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleInputQuery = (evt) => {
+        setSearchQuery(evt.target.value)
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        setSearchQuery('');
+        console.log('search:', searchQuery);
+    }
+
+    return (
+        <search className="index-hero__search">
+            <form className="index-hero__search--form" method="post" onSubmit={handleSubmit}>
+                <label>
+                    <input onChange={handleInputQuery} type="search" name="search" placeholder={searchPlaceholder} value={searchQuery}/>
+                </label>
+                <button type="submit" title="Пошук">
+                    <svg width="26" height="26" viewBox="0 0 48 48">
+                        <use xlinkHref="#search"></use>
+                    </svg>
+                </button>
+            </form>
+        </search>
+    )
 }
