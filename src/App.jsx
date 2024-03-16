d10ded436a36cbaecda347d1888522de0ac49ea6
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-03-16 16:37:46 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import './assets/js/main'
 import './assets/scss/style.scss'
 import { routes } from './routes/routes'
@@ -7,25 +7,51 @@ import { Header } from './components/Header/Header';
 import { Sprite } from './components/IconSprite/Sprite';
 import { Footer } from './components/Footer/Footer';
 import { LoginPopUp } from './components/LoginPopUp/LoginPopUp';
+// import { RegisterPopUp } from './components/RegisterPopUp/RegisterPopUp';
+
+export const PopupContext = createContext(null);
 
 export const App = () => {
   const [routesList] = useState(routes())
+  const [modal, setModal] = useState('');
+
+  console.log(modal);
+
+  // const ModalList = {
+  //   'login': <LoginPopUp/>,
+  //   'register': <RegisterPopUp/>,
+  // }
+
+  const ModalList = () => {
+    if (modal === 'login') {
+      return <LoginPopUp modal={modal}/>
+    }
+  }
+
+  console.log(ModalList);
 
   return (
     <>
       <Sprite/>
-    
-      <Header/>
+      
+      <PopupContext.Provider value={setModal}>
 
-      <div className='main'>
-        <Routes>
-          {routesList.map(route => <Route key={route.path} element={route.element} path={route.path}/>)}
-        </Routes>
-      </div>
+        <Header/>
 
-      <Footer/>
+        <div className='main'>
+          <Routes>
+            {routesList.map(route => <Route key={route.path} element={route.element} path={route.path}/>)}
+          </Routes>
+        </div>
 
-      <LoginPopUp/>
+        <Footer/>
+
+        {/* <LoginPopUp modal={modal}/> */}
+        {ModalList()}
+
+        {/* <RegisterPopUp/> */}
+      </PopupContext.Provider>
+
     </>
   );
 }
--- src/components/Header/components/HeaderMain/components/HeaderAddNavDesk/HeaderAddNavDesk.jsx
+++ src/components/Header/components/HeaderMain/components/HeaderAddNavDesk/HeaderAddNavDesk.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { PopupContext } from '../../../../../../App';
 
 export const HeaderAddNavDesk = () => {
 
-    
+    const handlePopUpClick = () => {
+        SetPopContext('login');
+    }
+
+
+    const SetPopContext = useContext(PopupContext);
 
     return (
         <div className="header__add-nav visible-on-desktop">
             <ul>
                 <li>
-                    <a href="#login-popup" className="open-popup" title="Акаунт">
+                    <a href="#login-popup" onClick={handlePopUpClick} className="open-popup" title="Акаунт">
                         <svg width="26" height="26" viewBox="0 0 48 48">
                             <use xlinkHref="#user"></use>
                         </svg>
