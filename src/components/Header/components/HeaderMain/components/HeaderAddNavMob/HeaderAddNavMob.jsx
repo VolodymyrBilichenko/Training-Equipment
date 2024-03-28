324a832802ff397d4f886cce1405cc5561bd67ce
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-03-28 14:15:37 +0200

    f

--- src/components/Header/components/HeaderMain/components/HeaderAddNavMob/HeaderAddNavMob.jsx
+++ src/components/Header/components/HeaderMain/components/HeaderAddNavMob/HeaderAddNavMob.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom';
+import getCookies from '../../../../../../functions/getCookies';
+import { PopupContext } from '../../../../../../App';
 
 export const HeaderAddNavMob = () => {
+    const navigate = useNavigate();
+
+
+    const handlePopUpClick = () => {        
+        if (getCookies('cookieToken')) {
+            navigate('/profile');
+        } else SetPopContext('login');
+    }
+
+    const SetPopContext = useContext(PopupContext);
   return (
     <div className="header__add-nav visible-on-mob">
         <ul>
             <li>
-                <a href="#login-popup" className="open-popup" title="Акаунт">
+                <button onClick={handlePopUpClick} className="open-popup" title="Акаунт">
                     <svg width="26" height="26" viewBox="0 0 48 48">
                         <use xlinkHref="#user"></use>
                     </svg>
-                </a>
+                </button>
             </li>
             <li>
-                <a href="favorites.html" title="Обране">
+                <NavLink to={'/favorites'} title="Обране">
                     <svg width="26" height="26" viewBox="0 0 48 48">
                         <use xlinkHref="#favorite"></use>
                     </svg>
-                </a>
+                </NavLink>
             </li>
             <li>
-                <a href="cart.html" title="Кошик">
+                <NavLink to={'/basket'} title="Кошик">
                     <svg width="26" height="26" viewBox="0 0 48 48">
                         <use xlinkHref="#cart"></use>
                     </svg>
-                </a>
+                </NavLink>
             </li>
         </ul>
     </div>
