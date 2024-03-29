752364c30df4584e01172fceab0fb914e7258324
Vlas <vlas.zub@gmail.com>
2024-03-29 12:56:59 +0200

    Update CartList.jsx

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -11,10 +11,15 @@ export const CartList = ({products, setTotalAmount}) => {
 
     return (
         <ul className="cart__list" data-price-sum-container-id="cart-list">
-            {productsList?.filter(item => basketList.some(item2 => item.id === item2.product_id))?.map(productItem => (
+            {basketList.length ? productsList?.filter(item => basketList.some(item2 => item.id === item2.product_id))?.map(productItem => (
                 <CartListItem key={productItem.id} products={products} setTotalAmount={setTotalAmount}
                               productInfo={productItem}/>
-            ))}
+            )) : <>
+                <br/>
+                <br/>
+                <p>Корзина пустая</p>
+                <br/>
+            </>}
         </ul>
     )
 }
