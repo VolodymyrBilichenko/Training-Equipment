99e8e3420f66c0fa46f2f35571c6487180f805ff
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-04-06 12:24:49 +0300

    + w

--- src/components/Header/components/HeaderMain/components/HeaderAddNavDesk/HeaderAddNavDesk.jsx
+++ src/components/Header/components/HeaderMain/components/HeaderAddNavDesk/HeaderAddNavDesk.jsx
@@ -2,13 +2,17 @@ import React, { useContext } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { PopupContext } from '../../../../../../App';
 import getCookies from '../../../../../../functions/getCookies';
+import {useSelector} from 'react-redux'
 
 export const HeaderAddNavDesk = () => {
     const navigate = useNavigate();
+    const usersData = useSelector(state => state.toolkit.user)
+    const token = getCookies('cookieToken');
 
+    console.log(usersData);
 
     const handlePopUpClick = () => {        
-        if (getCookies('cookieToken')) {
+        if (token) {
             navigate('/profile');
         } else SetPopContext('login');
     }
@@ -20,11 +24,11 @@ export const HeaderAddNavDesk = () => {
         <div className="header__add-nav visible-on-desktop">
             <ul>
                 <li>
-                    <button onClick={handlePopUpClick} className="open-popup" title="Акаунт">
+                    <button onClick={handlePopUpClick} className="open-popup" title="Акаунт" style={token ? { gap: '4px'} : {gap: '0'}}>
                         <svg width="26" height="26" viewBox="0 0 48 48" style={{minWidth: '26px', minHeight: '26px'}}>
                             <use xlinkHref="#user"></use>
                         </svg>
-                        <span>Тарас Шевченко</span>
+                        <span>{usersData ? usersData.name : ''}</span>
                     </button>
                 </li>
                 <li>
