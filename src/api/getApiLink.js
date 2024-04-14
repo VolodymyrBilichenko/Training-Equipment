f1d93fcac4aa49b96a9061e53d32845270b1ba09
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-04-14 14:17:38 +0300

    Update getApiLink.js

--- src/api/getApiLink.js
+++ src/api/getApiLink.js
@@ -1,3 +1,4 @@
 export const getApiLink = (path) => {
-    return 'https://69bb-91-128-137-72.ngrok-free.app' + path
+    // return 'https://69bb-91-128-137-72.ngrok-free.app' + path
+    return 'https://education-equipment.roadwise.info' + path
 }
